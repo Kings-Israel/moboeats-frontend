5fdcd5fd6cbad89ed3af32b3738060708f78d7ad
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-10-05 09:12:47 +0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   define: {
     'process.env': process.env
   },
+  base: process.env.NODE_ENV == 'production' ? 'http://194.163.129.200/moboeats-frontend/' : '',
   plugins: [vue()],
   resolve: {
     alias: [
@@ -22,7 +23,7 @@ export default defineConfig({
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
-    }
+    },
   },
   server: {
     host: 'admin.moboeats.test',
