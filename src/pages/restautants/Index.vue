af10809ce442476ff4f8e1d1a5f02063a4595a78
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-11-26 13:09:54 +0300

    Update

--- src/pages/restautants/Index.vue
+++ src/pages/restautants/Index.vue
@@ -22,21 +22,32 @@
             </div>
 
             <!-- Right: Actions  -->
-            <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
+            <button class="bg-emerald-600 px-2 py-1  col-span-1 text-slate-50 font-semibold rounded-full justify-end" @click="exportRestaurants()">Export Restaurants</button>
 
-              <form class="relative">
-                <label for="action-search" class="sr-only">Search</label>
-                <input id="action-search" class="form-input pl-9 bg-white dark:bg-slate-800" type="search" v-model="search" placeholder="Search Restaurants" />
-                <button class="absolute inset-0 right-auto group" type="submit" aria-label="Search">
-                  <svg class="w-4 h-4 shrink-0 fill-current text-slate-400 dark:text-slate-500 group-hover:text-slate-500 dark:group-hover:text-slate-400 ml-3 mr-2" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M7 14c-3.86 0-7-3.14-7-7s3.14-7 7-7 7 3.14 7 7-3.14 7-7 7zM7 2C4.243 2 2 4.243 2 7s2.243 5 5 5 5-2.243 5-5-2.243-5-5-5z" />
-                    <path d="M15.707 14.293L13.314 11.9a8.019 8.019 0 01-1.414 1.414l2.393 2.393a.997.997 0 001.414 0 .999.999 0 000-1.414z" />
-                  </svg>
-                </button>
-              </form>
+            <!-- Right: Actions  -->
+          </div>
 
-            </div>
-
+          <div class="grid grid-cols-3 mb-2">
+            <form class="relative">
+              <label for="action-search" class="sr-only">Search</label>
+              <input id="action-search" class="form-input w-full pl-9 bg-white border border-slate-500 dark:bg-slate-800" type="search" v-model="search" placeholder="Search Restaurants" />
+              <button class="absolute inset-0 right-auto group" type="submit" aria-label="Search">
+                <svg class="w-4 h-4 shrink-0 fill-current text-slate-400 dark:text-slate-500 group-hover:text-slate-500 dark:group-hover:text-slate-400 ml-3 mr-2" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M7 14c-3.86 0-7-3.14-7-7s3.14-7 7-7 7 3.14 7 7-3.14 7-7 7zM7 2C4.243 2 2 4.243 2 7s2.243 5 5 5 5-2.243 5-5-2.243-5-5-5z" />
+                  <path d="M15.707 14.293L13.314 11.9a8.019 8.019 0 01-1.414 1.414l2.393 2.393a.997.997 0 001.414 0 .999.999 0 000-1.414z" />
+                </svg>
+              </button>
+            </form>
+            <form class="relative w-full">
+              <label for="action-search" class="sr-only">Search Status</label>
+              <select class="form-select p-2 w-full border border-slate-500" v-model="status">
+                <option value="">All</option>
+                <option value="2">Approved</option>
+                <option value="1">Pending</option>
+                <option value="3">Denied</option>
+              </select>
+            </form>
+            <button class="bg-red-500 text-white rounded-full col-span-1 h-fit py-1 my-auto justify-self-end w-72" @click="search = '', status = ''">Clear Field</button>
           </div>
 
           <!-- Table -->
@@ -169,6 +180,8 @@ export default {
 
     let search = ref('')
 
+    let status = ref('')
+
     const updateSelectedItems = (selected) => {
       selectedItems.value = selected
     }
@@ -233,22 +246,73 @@ export default {
     const resolveRestaurantStatus = (status) => {
       switch (status) {
         case 'Pending':
-          return 'text-gray-100 bg-gray-500'
+          return 'text-gray-100 bg-gray-500 px-2'
           break;
         case 'Denied':
-          return 'text-gray-100 bg-red-500'
+          return 'text-gray-100 bg-red-500 px-2'
           break;
         case 'Approved':
-          return 'text-green-100 bg-green-500'
+          return 'text-green-100 bg-green-500 px-2'
           break;
         default:
-          return 'text-gray-100 bg-gray-500'
+          return 'text-gray-100 bg-gray-500 px-2'
           break;
       }
     }
 
+    const exportRestaurants = () => {
+      $http.get('/restaurant/restaurants/export/data?search='+search.value+'/&status='+status.value,
+          {
+            responseType: 'arraybuffer',
+            method: 'GET'
+          }
+      )
+        .then(response => {
+          const fileURL = window.URL.createObjectURL(new Blob([response.data]))
+          const fileLink = document.createElement('a')
+
+          fileLink.href = fileURL
+          fileLink.setAttribute(
+            'download',
+            `Restaurants.xlsx`,
+          )
+          document.body.appendChild(fileLink)
+
+          fileLink.click()
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+
     watch(search, async (newSearch, oldQuestion) => {
       $http.get('/admin/restaurants?search='+newSearch)
+        .then(response => {
+          nextPageUrl.value = response.data.data.next_page_url
+          lastPageUrl.value = response.data.data.last_page_url
+          prevPageUrl.value = response.data.data.prev_page_url
+          totalItems.value = response.data.data.total
+          from.value = response.data.data.from
+          to.value = response.data.data.to
+          restaurants.value = []
+          response.data.data.data.forEach(restaurant => {
+            restaurants.value.push({
+              id: restaurant.id,
+              status: restaurant.status,
+              uuid: restaurant.uuid,
+              logo: restaurant.logo,
+              name: restaurant.name,
+              user: restaurant.user,
+              location: restaurant.address,
+              orders: restaurant.orders.length,
+              lastOrder: restaurant.orders.length > 0 ? getOrderId(restaurant.orders[restaurant.orders.length - 1].uuid) : '-',
+            })
+          })
+        })
+    })
+
+    watch(status, async (newStatus, oldQuestion) => {
+      $http.get('/admin/restaurants?status='+newStatus)
         .then(response => {
           nextPageUrl.value = response.data.data.next_page_url
           lastPageUrl.value = response.data.data.last_page_url
@@ -286,7 +350,9 @@ export default {
       to,
       changePage,
       search,
+      status,
       resolveRestaurantStatus,
+      exportRestaurants
     }  
   }
 }
