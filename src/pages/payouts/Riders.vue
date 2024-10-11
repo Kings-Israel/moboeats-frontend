04863ee22c389a975546d9daf3bd3c5d8b6019c5
kings <milimokings@gmail.com>
2024-10-11 16:12:30 +0300

    Update

--- src/pages/payouts/Riders.vue
+++ src/pages/payouts/Riders.vue
@@ -79,19 +79,19 @@
                     <tr v-for="payout in payouts.data" :key="payout.id">
                       <td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
                         <div class="flex items-center">
-                          <div class="w-10 h-10 shrink-0 mr-2 sm:mr-3" v-if="payout.payable && payout.payable.image">
+                          <div class="w-10 h-10 shrink-0 mr-2 sm:mr-3" v-if="payout.payable && (payout.payable.image || payout.payable.name)">
                             <img class="rounded-full" :src="payout.payable.image" width="40" height="40" :alt="payout.payable.name" />
                           </div>
-                          <div class="font-medium text-slate-800 dark:text-slate-100">{{payout.payable.name}}</div>
+                          <div class="font-medium text-slate-800 dark:text-slate-100" v-if="payout.payable && payout.payable.name">{{payout.payable.name}}</div>
                         </div>
                       </td>
                       <td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                        <div class="text-left">{{payout.payable.email}}</div>
+                        <div class="text-left" v-if="payout.payable && payout.payable.email">{{payout.payable.email}}</div>
                       </td>
                       <td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                        <div class="text-left font-medium flex w-fit px-2 py-1 rounded-md text-green-500">{{formatValue(payout.amount, payout.payable.country)}}</div>
+                        <div class="text-left font-medium flex w-fit px-2 py-1 rounded-md text-green-500" v-if="payout.payable && payout.payable.country">{{formatValue(payout.amount, payout.payable.country)}}</div>
                       </td>
-                      <td class="px-2 py-3 whitespace-nowrap w-px flex gap-1">
+                      <td class="px-2 py-3 whitespace-nowrap w-px flex gap-1" v-if="payout.payable">
                         <router-link class="btn bg-indigo-500 hover:bg-indigo-600 text-white btn-sm" :to="{ name: 'rider-details', 'params': { id: payout.payable.id, type: 'rider' }}">View</router-link>
                       </td>
                     </tr>
