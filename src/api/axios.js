8eb110c5e0d34e3b334d73ae99e6120b0e90b743
Kings-Milimo <kings.milimo@yopesa.co>
2025-06-30 15:27:48 +0300

    Update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,11 @@
 // axios
-import axios from 'axios'
-import router from '../router'
+import axios from "axios";
+import router from "../router";
 
-const baseURL = process.env.NODE_ENV === 'development' ? 'http://api.moboeats.test/api/v1/' : 'https://api.moboeats.co.uk/api/v1'
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://api.moboeats.test/api/v1/"
+    : "https://api.moboeats.com/api/v1";
 
 const axiosIns = axios.create({
   // You can add your headers here
@@ -10,51 +13,50 @@ const axiosIns = axios.create({
   baseURL,
   // timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
-})
+});
 
 //  Set the AUTH token for any request
 axiosIns.interceptors.request.use(function (config) {
-  const token = localStorage.getItem('token');
-  if(token){
-    config.headers.Authorization =  token ? `Bearer ${token}` : '';
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = token ? `Bearer ${token}` : "";
   }
   return config;
 });
 
 //Intercept every response to check for errors
 axiosIns.interceptors.response.use(null, (error) => {
-  switch(error.response.status) {
-      // case 400:
-      //     console.error(error.response.status, error.message);
-      //     Vue.$toast.open({
-      //         message: 'Unable to process the request. Kindly check your data',
-      //         type: 'error',
-      //         position: "top-right",
-      //         duration: 6000
-      //         // all other options may go here
-      //     });
-      //     break;
+  switch (error.response.status) {
+    // case 400:
+    //     console.error(error.response.status, error.message);
+    //     Vue.$toast.open({
+    //         message: 'Unable to process the request. Kindly check your data',
+    //         type: 'error',
+    //         position: "top-right",
+    //         duration: 6000
+    //         // all other options may go here
+    //     });
+    //     break;
 
-      case 401: // authentication error, logout the user
-          // Vue.$toast.open({
-          //     message: 'Authentication error / Unauthorized',
-          //     type: 'error',
-          //     position: "top-right",
-          //     duration: 4000
-          //     // all other options may go here
-          // });
-          // //destroy token, cause user may have faked role in local storage
-          localStorage.removeItem("token");
-          localStorage.removeItem("user");
-          router.push('/signin');
-          break;
+    case 401: // authentication error, logout the user
+      // Vue.$toast.open({
+      //     message: 'Authentication error / Unauthorized',
+      //     type: 'error',
+      //     position: "top-right",
+      //     duration: 4000
+      //     // all other options may go here
+      // });
+      // //destroy token, cause user may have faked role in local storage
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      router.push("/signin");
+      break;
 
-      default:
-          console.error(error.response.status, error.message);
-          // notify.error('Server Error');
-
+    default:
+      console.error(error.response.status, error.message);
+    // notify.error('Server Error');
   }
-return Promise.reject(error);
+  return Promise.reject(error);
 });
 
-export default axiosIns
+export default axiosIns;
